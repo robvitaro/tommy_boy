dbed8ace4e69dd0b3e105b79dfcf37b30e698fff
Rob Vitaro <robvitaro@gmail.com>
2023-11-09 11:48:58 -0500

    basic bootstrap layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when :notice, 'notice' then "alert alert-info"
+    when :success, 'success' then "alert alert-success"
+    when :error, 'error' then "alert alert-error"
+    when :alert, 'alert' then "alert alert-error"
+    end
+  end
 end
