c4bde3bc1824413a8090d51aab38826ccedf6278
Rob Vitaro <robvitaro@gmail.com>
2023-11-13 14:33:20 -0500

    make Customer category an enum; validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
+  enum category: {residential: 0, commercial: 1}
   validates :name, presence: true
+  validates :category, presence: true, inclusion: { in: categories.keys }
 end
